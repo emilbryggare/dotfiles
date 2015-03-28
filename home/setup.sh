a7b123216f353d6cb49d533f59409c0b6e5fd826
Emil Bryggare <emil@emilbryggare.com>
2015-03-28 15:35:38 +0100

    Revert "Changed $home to ~"
    This reverts commit a8339f3cb359ddab2170502376cbe8740779febf.

--- home/setup.sh
+++ home/setup.sh
@@ -1,15 +1,15 @@
 # Setup Homeshick
-if [ ! -d "~/.homesick/repos/homeshick" ]
+if [ ! -d "$HOME/.homesick/repos/homeshick" ]
 then
-		git clone git://github.com/andsens/homeshick.git ~/.homesick/repos/homeshick
+		git clone git://github.com/andsens/homeshick.git $HOME/.homesick/repos/homeshick
 fi
-source "~/.homesick/repos/homeshick/homeshick.sh"
+source "$HOME/.homesick/repos/homeshick/homeshick.sh"
 homeshick clone emilbryggare/dotfiles
 homeshick symlink
-source "~/.bashrc"
+source "$HOME/.bashrc"
 
 # Install Vundle for VIM
-if [ ! -d "~/.vim/bundle/Vundle.vim" ]
+if [ ! -d "$HOME/.vim/bundle/Vundle.vim" ]
 then
 	git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 fi
